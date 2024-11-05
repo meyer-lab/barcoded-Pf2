3262a99b425dcaeeab80a294dc99fd53727c05d9
Aaron Meyer <ameyer@ucla.edu>
2024-11-05 06:27:34 -0800

    rye format

--- figure2.py
+++ figure2.py
@@ -1,13 +1,13 @@
-#%%
+# %%
 import scanpy as sc
 from pf2barcode.imports import import_CCLE
 import seaborn as sns
 
 X = import_CCLE()
 
-Xsel = X[:, X.var.index.str.contains('AXL')]
+Xsel = X[:, X.var.index.str.contains("AXL")]
 Xsel.uns
 
-sc.pl.pca(X, color="SW", components=['1, 2'], size=10)
+sc.pl.pca(X, color="SW", components=["1, 2"], size=10)
 
 # %%
