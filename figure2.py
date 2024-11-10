53ae8e2fe42d89e0b5475dc264f763ea7fdf791f
MelissaMouchamel <melissamouchamel@gmail.com>
2024-11-10 12:29:57 -0800

    Added figure2.py

---
+++ figure2.py
@@ -0,0 +1,13 @@
+# %%
+import scanpy as sc
+from pf2barcode.imports import import_CCLE
+import seaborn as sns
+
+X = import_CCLE()
+
+Xsel = X[:, X.var.index.str.contains("AXL")]
+Xsel.uns
+
+sc.pl.pca(X, color="SW", components=["1, 2"], size=10)
+
+# %%
