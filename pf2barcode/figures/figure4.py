598874efb7a2a2901a47a81bf268cb6b571bec5a
Aaron Meyer <ameyer@ucla.edu>
2024-11-13 12:28:20 -0800

    Rye fmt

--- pf2barcode/figures/figure4.py
+++ pf2barcode/figures/figure4.py
@@ -1,17 +1,17 @@
-import seaborn as sns
-import numpy as np
-from scipy.stats import kruskal
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from scipy.stats import kruskal
 
 from pf2barcode.imports import import_CCLE
 
 from .common import (
     getSetup,
     subplotLabel,
-)                                        
+)
+
 
 def makeFigure():
-
     X = import_CCLE()
 
     """Get a list of the axis objects and create a figure."""
@@ -32,5 +32,5 @@ def makeFigure():
     sns.barplot(x=np.arange(pvalues.shape[0]), y=-np.log10(pvalues))
     plt.xlabel("PC")
     plt.ylabel("-log10(p-value)")
-    
+
     return f
