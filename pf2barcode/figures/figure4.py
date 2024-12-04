05b93e0b8f9e56c0cd196c3282d6aed5cec6e3fa
jolinahor <jolinahor@g.ucla.edu>
2024-12-04 10:48:21 -0800

    Debugged Figure 4 Code

--- pf2barcode/figures/figure4.py
+++ pf2barcode/figures/figure4.py
@@ -1,7 +1,7 @@
-import seaborn as sns
-import numpy as np
-from scipy.stats import kruskal
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from scipy.stats import kruskal
 
 from pf2barcode.imports import import_CCLE
 
@@ -14,20 +14,19 @@ def makeFigure():
 
     X = import_CCLE()
 
-    """Get a list of the axis objects and create a figure."""
     ax, f = getSetup((10, 6), (1, 1))
     subplotLabel(ax)
 
-    idx_selected = 0
-    cells = []
     pvalues = np.zeros(X.obsm["X_pca"].shape[1])
 
     for jj in range(X.obsm["X_pca"].shape[1]):
+        cells = []
         for barcodes in X.obs["SW"].unique():
-            cells_selected = X.obsm["X_pca"][X.obs["SW"] == barcodes, idx_selected]
+            cells_selected = X.obsm["X_pca"][X.obs["SW"] == barcodes, jj]
             cells.append(cells_selected.flatten())
 
         pvalues[jj] = kruskal(*cells).pvalue
+        
 
     sns.barplot(x=np.arange(pvalues.shape[0]), y=-np.log10(pvalues))
     plt.xlabel("PC")
