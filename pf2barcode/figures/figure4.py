868278d879b7aa285680ccef5db3159dbabadd3c
jolinahor <jolinahor@g.ucla.edu>
2025-01-12 17:52:08 -0800

    Added comments to code

--- pf2barcode/figures/figure4.py
+++ pf2barcode/figures/figure4.py
@@ -10,24 +10,33 @@ from .common import (
     subplotLabel,
 )
 
-
 def makeFigure():
     X = import_CCLE()
-
+    
+    # Get a list of the axis objects and create a figure.
     ax, f = getSetup((10, 6), (1, 1))
     subplotLabel(ax)
 
+    #  Initialize an array of zeros to store p-values for each PC
     pvalues = np.zeros(X.obsm["X_pca"].shape[1])
 
+    # Loop through each principal component (PC) to compute p-values
     for jj in range(X.obsm["X_pca"].shape[1]):
-        cells = []
+        cells = [] 
+
+        # Loop through each unique barcode in the "SW" observation column
         for barcodes in X.obs["SW"].unique():
+
+            # Select data corresponding to current iteration of barcode and PC
             cells_selected = X.obsm["X_pca"][X.obs["SW"] == barcodes, jj]
+
+            # Flatten data and add to list
             cells.append(cells_selected.flatten())
 
+        # Perform the Kruskal-Wallis H-test
         pvalues[jj] = kruskal(*cells).pvalue
         
-
+    # Barplot setup
     sns.barplot(x=np.arange(pvalues.shape[0]), y=-np.log10(pvalues))
     plt.xlabel("PC")
     plt.ylabel("-log10(p-value)")
