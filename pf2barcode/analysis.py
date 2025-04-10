047e1e49d7799e5aa8e7392bd5d535bb91df4430
Quincy McCall <167721850+quinn-mccall@users.noreply.github.com>
2025-04-10 10:47:10 -0700

    first draft of the anova pvalue function + testing. (#17)
    * first draft of the anova pvalue function + testing.
    
    * Lint and fix tests
    
    * Remove figure outputs
    
    * Fix imports
    
    * Fix build
    
    * Fix linting
    
    ---------
    
    Co-authored-by: Aaron Meyer <ameyer@ucla.edu>

--- pf2barcode/analysis.py
+++ pf2barcode/analysis.py
@@ -3,6 +3,9 @@ This file contains the kruskal_pvalues function that is used in figure 4 and 5.
 """
 
 import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 from scipy.stats import kruskal
 
 
@@ -21,5 +24,27 @@ def kruskal_pvalues(X):
             cells.append(cells_selected.flatten())
         # Perform the Kruskal-Wallis H-test
         pvalues[jj] = kruskal(*cells).pvalue
+
+    return pvalues
+
+
+def anova_pvalues(X):
+    # get the number of PCs used
+    n_pcs = X.obsm["X_pca"].shape[1]
+    # initialize an array of empty P-values for each PC
+    pvalues = np.zeros(n_pcs)
+
+    # loop over every PC
+    for jj in range(n_pcs):
+        # construct a pandas dataframe with the PC and the associated group label
+        df = pd.DataFrame({"PC": X.obsm["X_pca"][:, jj], "Group": X.obs["SW"].values})
+
+        # apply ordinary least squares to model the PC values as a function of
+        # group membership
+        model = smf.ols("PC ~ Group", data=df).fit()
+        # apply anova to the model
+        anova_table = sm.stats.anova_lm(model, typ=2)
+        # assign the p values from the anova table
+        pvalues[jj] = anova_table["PR(>F)"]["Group"]
 
     return pvalues
--- pf2barcode/figures/figure4.py
+++ pf2barcode/figures/figure4.py
@@ -4,20 +4,19 @@ negative log10 p-values from the Kruskal-Wallis H-test
 
 Computed p-values determines if distributions of PCs are statistically
 significantly across different groups, and the negative log10 transformation
-of the p-values allows for easier identification and interpretation of signficant PCs 
+of the p-values allows for easier identification and interpretation of signficant PCs
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-from pf2barcode.imports import import_CCLE
-
-from ..analysis import kruskal_pvalues
-from .common import (
+from pf2barcode.analysis import anova_pvalues, kruskal_pvalues
+from pf2barcode.figures.common import (
     getSetup,
     subplotLabel,
 )
+from pf2barcode.imports import import_CCLE
 
 
 def makeFigure():
@@ -29,6 +28,24 @@ def makeFigure():
 
     # Implement kruskal_pvalues function
     pvalues = kruskal_pvalues(X)
+
+    # Barplot setup
+    sns.barplot(x=np.arange(pvalues.shape[0]), y=-np.log10(pvalues))
+    plt.xlabel("PC")
+    plt.ylabel("-log10(p-value)")
+
+    return f
+
+
+def makeFigureAnova():
+    X = import_CCLE()
+
+    # Get a list of the axis objects and create a figure.
+    ax, f = getSetup((10, 6), (1, 1))
+    subplotLabel(ax)
+
+    # Implement anova_pvalues function
+    pvalues = anova_pvalues(X)
 
     # Barplot setup
     sns.barplot(x=np.arange(pvalues.shape[0]), y=-np.log10(pvalues))
